209a781fb0afc05f2470e3fb08f62cacae7585e0
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2024-02-24 13:51:20 +0200

    Delete firebase.ts

--- src/database/firebase.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAFsukXGzpODY0QttmOF5XSbBPqjpk4z_g",
-  authDomain: "chessroyale-3e15a.firebaseapp.com",
-  projectId: "chessroyale-3e15a",
-  storageBucket: "chessroyale-3e15a.appspot.com",
-  messagingSenderId: "782230085124",
-  appId: "1:782230085124:web:ee569029ab913b64285f51",
-  measurementId: "G-YY37HCL6P7"
-};
