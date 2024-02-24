b5fb0a4aa403bebee1565e12ca272dfb96a59db9
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2024-02-24 16:41:03 +0200

    websockets are getting to the server but not to each other

--- src/ChessBoardComponents/Chessboard/Chessboard.tsx
+++ src/ChessBoardComponents/Chessboard/Chessboard.tsx
@@ -1,15 +1,17 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import "./Chessboard.css";
 import Tile from "../Tile/Tile";
 import { VERTICAL_AXIS, HORIZONTAL_AXIS, GRID_SIZE } from "../../Constants";
 import { Piece, Position } from "../../models";
+import { io, Socket } from "socket.io-client";
 
 interface Props {
   playMove: (piece: Piece, position: Position) => boolean;
   pieces: Piece[];
+  socket: Socket | null; // Add a prop for socket
 }
 
-export default function Chessboard({ playMove, pieces }: Props) {
+export default function Chessboard({ playMove, pieces, socket }: Props) {
   const [activePiece, setActivePiece] = useState<HTMLElement | null>(null);
   const [grabPosition, setGrabPosition] = useState<Position>(
     new Position(-1, -1)
@@ -34,12 +36,18 @@ export default function Chessboard({ playMove, pieces }: Props) {
       element.style.top = `${y}px`;
 
       setActivePiece(element);
+
+      // Emit grabbing action to synchronize with other players
+      if (socket && grabPosition.x !== -1 && grabPosition.y !== -1) {
+        socket.emit("grab", { grabX: grabPosition.x, grabY: grabPosition.y });
+      }
     }
   }
 
   function movePiece(e: React.MouseEvent) {
     const chessboard = chessboardRef.current;
     if (activePiece && chessboard) {
+      // Calculate the boundaries for piece movement
       const minX = chessboard.offsetLeft - 25;
       const minY = chessboard.offsetTop - 25;
       const maxX = chessboard.offsetLeft + chessboard.clientWidth - 75;
@@ -48,33 +56,20 @@ export default function Chessboard({ playMove, pieces }: Props) {
       const y = e.clientY - 50;
       activePiece.style.position = "absolute";
 
-      //If x is smaller than minimum amount
+      // Ensure the piece stays within the boundaries
       if (x < minX) {
         activePiece.style.left = `${minX}px`;
-      }
-
-      //If x is bigger than maximum amount
-      else if (x > maxX) {
+      } else if (x > maxX) {
         activePiece.style.left = `${maxX}px`;
-      }
-
-      //If x is in the constraints
-      else {
+      } else {
         activePiece.style.left = `${x}px`;
       }
 
-      //If y is smaller than minimum amount
       if (y < minY) {
         activePiece.style.top = `${minY}px`;
-      }
-
-      //If y is bigger than maximum amount
-      else if (y > maxY) {
+      } else if (y > maxY) {
         activePiece.style.top = `${maxY}px`;
-      }
-
-      //If y is in the constraints
-      else {
+      } else {
         activePiece.style.top = `${y}px`;
       }
     }
@@ -91,10 +86,15 @@ export default function Chessboard({ playMove, pieces }: Props) {
       const currentPiece = pieces.find((p) => p.samePosition(grabPosition));
 
       if (currentPiece) {
-        let succes = playMove(currentPiece.clone(), new Position(x, y));
+        let success = playMove(currentPiece.clone(), new Position(x, y));
 
-        if (!succes) {
-          //RESETS THE PIECE POSITION
+        if (socket && grabPosition.x !== -1 && grabPosition.y !== -1) {
+          socket.emit("move", {
+            piece: currentPiece.clone(),
+            to: new Position(x, y)
+          });
+        } else {
+          // Reset piece position if move is not successful
           activePiece.style.position = "relative";
           activePiece.style.removeProperty("top");
           activePiece.style.removeProperty("left");
@@ -103,54 +103,77 @@ export default function Chessboard({ playMove, pieces }: Props) {
       setActivePiece(null);
     }
   }
+
   let listadder = [];
-  
+
   let pawnW = 'url("assets/images/pawn_w.png")';
   let pawnB = 'url("assets/images/pawn_b.png")';
-  let bishopW= 'url("assets/images/bishop_w.png")';
-  let bishopB= 'url("assets/images/bishop_b.png")';
-  let rookW= 'url("assets/images/rook_w.png")';
-  let rookB= 'url("assets/images/rook_b.png")';
-  let queenW= 'url("assets/images/queen_w.png")';
-  let queenB= 'url("assets/images/queen_b.png")';
-  let princeW= 'url("assets/images/prince_w.png")';
-  let princeB= 'url("assets/images/prince_b.png")';
-  let princessW= 'url("assets/images/princess_w.png")';
-  let princessB= 'url("assets/images/princess_b.png")';
-  let knightW= 'url("assets/images/knight_w.png")';
-  let knightB= 'url("assets/images/knight_b.png")';
-  let kingsW= 'url("assets/images/king_w.png")';
-  let kingsB= 'url("assets/images/king_b.png")';
-  
-  if (activePiece?.style.backgroundImage === pawnW || activePiece?.style.backgroundImage === pawnB) {
+  let bishopW = 'url("assets/images/bishop_w.png")';
+  let bishopB = 'url("assets/images/bishop_b.png")';
+  let rookW = 'url("assets/images/rook_w.png")';
+  let rookB = 'url("assets/images/rook_b.png")';
+  let queenW = 'url("assets/images/queen_w.png")';
+  let queenB = 'url("assets/images/queen_b.png")';
+  let princeW = 'url("assets/images/prince_w.png")';
+  let princeB = 'url("assets/images/prince_b.png")';
+  let princessW = 'url("assets/images/princess_w.png")';
+  let princessB = 'url("assets/images/princess_b.png")';
+  let knightW = 'url("assets/images/knight_w.png")';
+  let knightB = 'url("assets/images/knight_b.png")';
+  let kingsW = 'url("assets/images/king_w.png")';
+  let kingsB = 'url("assets/images/king_b.png")';
+
+  if (
+    activePiece?.style.backgroundImage === pawnW ||
+    activePiece?.style.backgroundImage === pawnB
+  ) {
     listadder.push("");
-      // console.log( "" );
-  }else if (activePiece?.style.backgroundImage === bishopW || activePiece?.style.backgroundImage === bishopB) {
+    // console.log( "" );
+  } else if (
+    activePiece?.style.backgroundImage === bishopW ||
+    activePiece?.style.backgroundImage === bishopB
+  ) {
     listadder.push("B");
-      // console.log("B");
-  }else if (activePiece?.style.backgroundImage === rookW || activePiece?.style.backgroundImage === rookB) {
+    // console.log("B");
+  } else if (
+    activePiece?.style.backgroundImage === rookW ||
+    activePiece?.style.backgroundImage === rookB
+  ) {
     listadder.push("R");
-      // console.log("R"); 
-  }else if (activePiece?.style.backgroundImage === queenW || activePiece?.style.backgroundImage === queenB) {
+    // console.log("R");
+  } else if (
+    activePiece?.style.backgroundImage === queenW ||
+    activePiece?.style.backgroundImage === queenB
+  ) {
     listadder.push("Q");
-      // console.log("Q");
-  }else if (activePiece?.style.backgroundImage === princeW || activePiece?.style.backgroundImage === princeB) {
+    // console.log("Q");
+  } else if (
+    activePiece?.style.backgroundImage === princeW ||
+    activePiece?.style.backgroundImage === princeB
+  ) {
     listadder.push("PC");
-      // console.log("PC");
-  }else if (activePiece?.style.backgroundImage === knightW || activePiece?.style.backgroundImage === knightB) {
+    // console.log("PC");
+  } else if (
+    activePiece?.style.backgroundImage === knightW ||
+    activePiece?.style.backgroundImage === knightB
+  ) {
     listadder.push("N");
-      // console.log("N");
-  }else if (activePiece?.style.backgroundImage === princessW || activePiece?.style.backgroundImage === princessB) {
+    // console.log("N");
+  } else if (
+    activePiece?.style.backgroundImage === princessW ||
+    activePiece?.style.backgroundImage === princessB
+  ) {
     listadder.push("PS");
-      // console.log("PS");
-  }else if (activePiece?.style.backgroundImage === kingsW || activePiece?.style.backgroundImage === kingsB) {
+    // console.log("PS");
+  } else if (
+    activePiece?.style.backgroundImage === kingsW ||
+    activePiece?.style.backgroundImage === kingsB
+  ) {
     listadder.push("K");
-      // console.log("K");
+    // console.log("K");
   }
-  
 
   let board = [];
-
 
   for (let j = VERTICAL_AXIS.length - 1; j >= 0; j--) {
     for (let i = 0; i < HORIZONTAL_AXIS.length; i++) {
@@ -159,12 +182,12 @@ export default function Chessboard({ playMove, pieces }: Props) {
       let image = piece ? piece.image : undefined;
 
       let currentSquare = String(VERTICAL_AXIS[j]) + String(HORIZONTAL_AXIS[i]);
-      
+
       let currentPiece =
-         activePiece != null
+        activePiece != null
           ? pieces.find((p) => p.samePosition(grabPosition))
           : undefined;
-        let highlight = currentPiece?.possibleMoves
+      let highlight = currentPiece?.possibleMoves
         ? currentPiece.possibleMoves.some((p) =>
             p.samePosition(new Position(i, j))
           )
@@ -179,15 +202,35 @@ export default function Chessboard({ playMove, pieces }: Props) {
           highlight={highlight}
         />
       );
-     
-      let test1 = Boolean(currentPiece?.position.y === Number(VERTICAL_AXIS[j]))
-      let test2 = Boolean(currentPiece?.position.x === Number(HORIZONTAL_AXIS[i]))
-      
+
+      let test1 = Boolean(
+        currentPiece?.position.y === Number(VERTICAL_AXIS[j])
+      );
+      let test2 = Boolean(
+        currentPiece?.position.x === Number(HORIZONTAL_AXIS[i])
+      );
+
       if (test1 && test2) {
         console.log("ssss");
       }
     }
   }
+
+  useEffect(() => {
+    if (socket) {
+      // Listen for grab events from other players
+      socket.on("grab", (data: { grabX: number; grabY: number }) => {
+        console.log("grabed!");
+        setGrabPosition(new Position(data.grabX, data.grabY));
+      });
+
+      // Listen for move events from other players
+      socket.on("move", (data: { piece: Piece; to: Position }) => {
+        console.log("moved!");
+        playMove(data.piece, data.to);
+      });
+    }
+  }, [socket, playMove]);
 
   return (
     <>
